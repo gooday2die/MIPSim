eaa12e0ecce63812f06c501be09238fbba4b3ecc
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2022-03-28 16:05:34 +0900

    Working on instructions

--- Utils/Instruction.cpp
+++ Utils/Instruction.cpp
@@ -12,6 +12,16 @@ Instruction::Instruction(const vector<uint32_t*>& argParameters) {
     this->parameters = argParameters;
 }
 
+void add_::execute() {
+    uint32_t rs = *Instruction::parameters.at(0);
+    uint32_t rt = *Instruction::parameters.at(1);
+    uint32_t* rd = Instruction::parameters.at(2);
+
+    if ((rs > 0) && (rt > INT_MAX - rs))
+        throw GeneralExceptions::OverflowException();
+    else *rd = rs + rt;
+}
+
 void addu_::execute() {
     uint32_t rs = *Instruction::parameters.at(0);
     uint32_t rt = *Instruction::parameters.at(1);
@@ -19,6 +29,60 @@ void addu_::execute() {
 
     *rd = rs + rt;
 }
+
+void and_::execute() {
+    uint32_t rs = *Instruction::parameters.at(0);
+    uint32_t rt = *Instruction::parameters.at(1);
+    uint32_t* rd = Instruction::parameters.at(2);
+    *rd = rs & rt;
+}
+
+void addi_::execute() {
+    uint32_t rs = *Instruction::parameters.at(0);
+    uint32_t* rt = Instruction::parameters.at(1);
+    uint32_t imm = *Instruction::parameters.at(2);
+
+    if ((rs > 0) && (imm > INT_MAX - rs))
+        throw GeneralExceptions::OverflowException();
+    else *rt = rs + imm;
+}
+
+void addiu_::execute() {
+    uint32_t rs = *Instruction::parameters.at(0);
+    uint32_t* rt = Instruction::parameters.at(1);
+    uint32_t imm = *Instruction::parameters.at(2);
+    *rt = rs + imm;
+}
+
+void andi_::execute() {
+    uint32_t rs = *Instruction::parameters.at(0);
+    uint32_t* rt = Instruction::parameters.at(1);
+    uint32_t imm = *Instruction::parameters.at(2);
+    *rt = rs & imm;
+}
+
+void nor_::execute() {
+    uint32_t rs = *Instruction::parameters.at(0);
+    uint32_t rt = *Instruction::parameters.at(1);
+    uint32_t* rd = Instruction::parameters.at(2);
+    *rd = ~(rs | rt);
+}
+
+void or_::execute() {
+    uint32_t rs = *Instruction::parameters.at(0);
+    uint32_t rt = *Instruction::parameters.at(1);
+    uint32_t* rd = Instruction::parameters.at(2);
+    *rd = (rs | rt);
+}
+
+void ori_::execute() {
+    uint32_t rs = *Instruction::parameters.at(0);
+    uint32_t* rt = Instruction::parameters.at(1);
+    uint32_t imm = *Instruction::parameters.at(2);
+    *rt = rs | imm;
+}
+
+
 
 void syscall_::execute() {
     uint32_t v0_val = *Instruction::parameters.at(0);
