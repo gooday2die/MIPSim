83025f49886ab619c7512eed2ab4e68758c2d302
Isu Kim <49092508+gooday2die@users.noreply.github.com>
2022-04-03 20:34:06 +0900

    Working on Simulator

---
+++ Simulator/Simulator.h
@@ -0,0 +1,25 @@
+//
+// @file : Simulator.h
+// @author : Gooday2die (Isu Kim)
+// @Contacts : edina00@naver.com
+// @brief : 
+// 
+
+#ifndef MIPSIM2_SIMULATOR_H
+#define MIPSIM2_SIMULATOR_H
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../Utils/Expression.h"
+
+class Simulator {
+private:
+    vector<Expression> textSectionExpressions;
+    
+};
+
+
+#endif //MIPSIM2_SIMULATOR_H
